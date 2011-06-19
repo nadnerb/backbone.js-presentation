bbb1c3ea9c67b6bda57a8775eee1bac637a7a3eb
Ronald Holshausen <uglyog@gmail.com>
2011-06-19 13:29:40 +1000

    linked page nav with next/back buttons

--- js/presentation.js
+++ js/presentation.js
@@ -14,6 +14,8 @@ window.PresentationController = Backbone.Controller.extend({
 
   page: function (pageIndex) {
     var page = parseInt(pageIndex, 10);
+    this.headerView.updatePage(page);
+    this.navigationView.updatePage(page);
   },
 
   loadPages: function () {
@@ -33,6 +35,8 @@ window.PresentationController = Backbone.Controller.extend({
         }
       });
     }
+
+    this.navigationView.initPages(page - 1);
   }
 
 });
@@ -41,10 +45,44 @@ window.NavigationView = Backbone.View.extend({
 
   el: '#navigation',
 
+  events: {
+    'click #back-button': 'back',
+    'click #next-button': 'next'
+  },
+
   render: function() {
     this.$("#back-button").button({ disabled: true });
     this.$("#next-button").button({ disabled: true });
+
     this.$("#progress-bar").progressbar();
+  },
+
+  updatePage: function (page) {
+    this.page = page;
+    if (page === 1) {
+      this.$("#back-button").button("disable");
+      this.$("#next-button").button("enable");
+    } else if (page === this.pages) {
+      this.$("#back-button").button("enable");
+      this.$("#next-button").button("disable");
+    } else {
+      this.$("#back-button").button("enable");
+      this.$("#next-button").button("enable");
+    }
+  },
+
+  initPages: function (pages) {
+    this.pages = pages;
+  },
+
+  back: function () {
+    var nextPage = this.page - 1;
+    document.location.hash = 'page/' + nextPage;
+  },
+
+  next: function () {
+    var nextPage = this.page + 1;
+    document.location.hash = 'page/' + nextPage;
   }
 
 });
@@ -54,6 +92,10 @@ window.HeaderView = Backbone.View.extend({
   el: '#header',
 
   render: function() {
+    
+  },
+
+  updatePage: function (page) {
     
   }
 
